62b5acc5233b5005195d3facc4f3ed57b956f3ce
Khurram Hashmi <hashmi.khurram786@gmail.com>
2025-12-19 02:19:58 +0100

    created conference plan event project

---
+++ src/venueSlice.js
@@ -0,0 +1,60 @@
+// venueSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+export const venueSlice = createSlice({
+  name: "venue",
+  initialState: [
+    {
+      img: "/images/venues/conference-room.png",
+      name: "Conference Room (Capacity:15)",
+      cost: 3500,
+      quantity: 0,
+    },
+    {
+      img: "/images/venues/autorium-hall.png",
+      name: "Auditorium Hall (Capacity:200)",
+      cost: 5500,
+      quantity: 0,
+    },
+    {
+      img: "/images/venues/presentation-room.png",
+      name: "Presentation Room (Capacity:50)",
+      cost: 700,
+      quantity: 0,
+    },
+    {
+      img: "/images/venues/large-meeting-room.png",
+      name: "Large Meeting Room (Capacity:10)",
+      cost: 900,
+      quantity: 0,
+    },
+    {
+      img: "/images/venues/small-meeting-room.png",
+      name: "Small Meeting Room (Capacity:5)",
+      cost: 1100,
+      quantity: 0,
+    },
+  
+  ],
+  reducers: {
+   
+    incrementQuantity: (state, action) => {
+      const { payload: index } = action;
+      if (state[index]) {
+        if (state[index].name === " Auditorium Hall (Capacity:200)" && state[index].quantity >= 3) {
+          return;        }
+        state[index].quantity++;
+      }
+    },
+    decrementQuantity: (state, action) => {
+      const { payload: index } = action;
+      if (state[index] && state[index].quantity > 0) {
+        state[index].quantity--;
+      }
+    },
+  },
+});
+
+export const { incrementQuantity, decrementQuantity } = venueSlice.actions;
+
+export default venueSlice.reducer;
